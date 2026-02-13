eb5750d5f1c518efa91bdc10002c0ba1967ed357
WilliamOdinson <109450999+WilliamOdinson@users.noreply.github.com>
2026-02-12 19:09:21 -0500

    feat: define service interface and connection constants

--- ticketbox/ticketbox_server.go
+++ ticketbox/ticketbox_server.go
@@ -2,5 +2,22 @@
 
 package main
 
+import (
+	"remote"
+)
+
+type TicketBoxInterface struct {
+	GetAllEvents func() ([]string, error, remote.RemoteError)
+	GetMyTickets func(user string) ([]string, error, remote.RemoteError)
+	BuyTicket    func(user string, event string) (string, error, remote.RemoteError)
+	RefundTicket func(user string, event string) (string, error, remote.RemoteError)
+}
+
+const (
+	address   = "localhost:14736"
+	isLossy   = true
+	isDelayed = true
+)
+
 func main() {
 }
