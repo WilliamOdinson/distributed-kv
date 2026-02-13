a35363792d64a2ad7ccf50c36d7a0fa26973f49b
WilliamOdinson <109450999+WilliamOdinson@users.noreply.github.com>
2026-02-13 16:29:12 -0500

    refactor(caller): enhance comments for CallerStubCreator and clarify function behavior

--- remote/caller.go
+++ remote/caller.go
@@ -9,56 +9,21 @@ import (
 	"time"
 )
 
-// CallerStubCreator -- use reflection to populate the interface functions to create the
-// caller's stub interface. Only works if all functions are exported. Once created,
-// the interface masks remote calls to a CalleeStub that hosts the object instance that
-// the functions are invoked on.  The network address of the remote CalleeStub must be
-// provided with the stub is created, and it may not change later. Arguments include:
-// -- a struct of function declarations to act as the stub's interface
-// -- the remote address of the CalleeStub as "<ip-address>:<port-number>"
-// -- indicator of whether caller-to-callee channel has emulated packet loss
-// -- indicator of whether caller-to-callee channel has emulated propagation delay
-// Performs the following:
-// -- returns a local error if function struct is nil
-// -- returns a local error if any function in the struct is not a remote function
-// -- otherwise, uses reflection to access the functions in the given struct and
+// CallerStubCreator uses reflection to populate the interface functions to create the
+// caller's stub interface. Once created, the interface masks remote calls to a CalleeStub
+// that hosts the object instance that the functions are invoked on.
 //
-//	populate their function definitions with the required CallerStub functionality
+// It checks the following conditions:
+// 1. serviceInterface must be a struct or pointer to struct type, and all fields must be functions.
+// 2. All functions must be exported and have RemoteError as their last return type (is a remote function).
 func CallerStubCreator(serviceInterface any, address string, isLossy bool, isDelayed bool) error {
-	// if serviceInterface is a pointer to a struct with function declarations,
-	// then reflect.TypeOf(serviceInterface).Elem() is the reflected struct's reflect.Type
-	// and reflect.ValueOf(serviceInterface).Elem() is the reflected struct's reflect.Value
-	//
-	// Here's what it needs to do (not strictly in this order):
-	//
-	//    1. create a request message populated with the method name and input
-	//       arguments to send to the CalleeStub
-	//
-	//    2. create a []reflect.Value of correct size to hold the result to be
-	//       returned back to the program
-	//
-	//    3. connect to the CalleeStub's tcp server, and wrap the connection in an
-	//       appropriate LeakySocket using the parameters given to the CallerStubCreator
-	//
-	//    4. encode the request message into a byte-string to send over the connection
-	//
-	//    5. send the encoded message, noting that the LeakySocket is not guaranteed
-	//       to succeed depending on the given parameters
-	//
-	//    6. wait for a reply to be received using Recv, which is blocking
-	//        -- if Recv returns an error, populate and return error output
-	//
-	//    7. decode the received byte-string according to the expected return types
-
 	// returns a local error if function struct is nil
 	if serviceInterface == nil {
 		return fmt.Errorf("serviceInterface should not be nil")
 	}
 
-	// returns a local error if any function in the struct is not a remote function
+	// if the serviceInterface is a pointer to a struct, first dereference to get the element type
 	serviceType := reflect.TypeOf(serviceInterface)
-
-	// dereference pointer types to get to the struct type
 	if serviceType.Kind() == reflect.Pointer {
 		serviceType = serviceType.Elem()
 	}
@@ -67,26 +32,28 @@ func CallerStubCreator(serviceInterface any, address string, isLossy bool, isDel
 		return fmt.Errorf("service interface must be a struct or pointer to struct")
 	}
 
+	// Check 1. the functions must be exported and have RemoteError as their last return type.
 	for i := 0; i < serviceType.NumField(); i++ {
 		field := serviceType.Field(i)
 		// check if the field is a function
 		if field.Type.Kind() != reflect.Func {
 			return fmt.Errorf("field %s is not a function", field.Name)
 		}
-		// check if the function has the correct signature
+		// check if the function's last return type is RemoteError
 		if field.Type.NumOut() == 0 || field.Type.Out(field.Type.NumOut()-1) != reflect.TypeOf(RemoteError{}) {
-			return fmt.Errorf("function %s does not have the correct signature", field.Name)
+			return fmt.Errorf("function %s does not have the correct signature, last return type must be RemoteError{}", field.Name)
 		}
 	}
 
+	// Main Part: for each function field, create a function implementation that performs the remote call.
 	for i := 0; i < serviceType.NumField(); i++ {
 		field := serviceType.Field(i)
 		funcName := field.Name
 
-		// create a function that matches the signature of the field
+		// create a function (stub) that matches the signature of the field
 		funcType := field.Type
 		funcImpl := reflect.MakeFunc(funcType, func(args []reflect.Value) []reflect.Value {
-			// build request message
+			// Step 1. create a request message instance, populated with the method name and input arguments
 			reqMsg := RequestMsg{
 				Method: funcName,
 				Args:   make([][]byte, len(args)),
@@ -99,11 +66,14 @@ func CallerStubCreator(serviceInterface any, address string, isLossy bool, isDel
 				reqMsg.Args[i] = buf.Bytes()
 			}
 
+			// Step 2: encode the request message into a byte-string with gob encoding
 			var reqBuf bytes.Buffer
 			if err := gob.NewEncoder(&reqBuf).Encode(reqMsg); err != nil {
 				return makeCallerErrorResponse(funcType, fmt.Sprintf("[Caller] failed to encode request message: %v", err))
 			}
 
+			// Step 3: connect to the CalleeStub's tcp server, and wrap the connection in an LeakySocket. Since the LeakySocket may 'fail' to send packets, we simply retry sending the request until we get a reply from the server.
+			// indicate whether the request has been successfully sent and a reply has been received
 			flag := false
 			var reply ReplyMsg
 
@@ -117,32 +87,38 @@ func CallerStubCreator(serviceInterface any, address string, isLossy bool, isDel
 				// wrap connection in leaky socket
 				leakyConn := NewLeakySocket(conn, isLossy, isDelayed)
 
-				// encode request message
+				// attempt to send; if the packet is dropped, retry with a new connection
 				sent, err := leakyConn.Send(reqBuf.Bytes())
 				if err != nil || !sent {
 					continue // packet lost, should retry
 				}
 
-				// wait for reply
+				// blocking (non-async) wait for reply with a 1-second read deadline; if no reply is received within the deadline, retry with a new connection wait for a reply
 				conn.SetReadDeadline(time.Now().Add(time.Second))
 				replyMsg, err := leakyConn.Recv()
 
 				if err != nil {
 					continue // packet lost, should retry
 				}
+
+				// decode the received byte-string according to the expected return types
 				if err := gob.NewDecoder(bytes.NewReader(replyMsg)).Decode(&reply); err != nil {
 					return makeCallerErrorResponse(funcType, fmt.Sprintf("[Caller] failed to decode reply message from remote callee: %v", err))
 				}
 				flag = true
 			}
 
+			// if the reply indicates a remote error, construct the return values with the error message.
 			if !reply.Success {
 				return makeCallerErrorResponse(funcType, reply.Err.Err)
 			}
+
+			// Step 4: decode the return values from the reply message according to the expected return types.
 			returnCount := funcType.NumOut()
 			results := make([]reflect.Value, returnCount)
 			for i := 0; i < returnCount-1; i++ {
 				outType := funcType.Out(i)
+				// rrror types are encoded as strings, so we need to decode the string and convert it back to an error type.
 				if outType.Implements(reflect.TypeOf((*error)(nil)).Elem()) {
 					errMessage := ""
 					if err := gob.NewDecoder(bytes.NewReader(reply.Reply[i])).Decode(&errMessage); err != nil {
@@ -152,8 +128,10 @@ func CallerStubCreator(serviceInterface any, address string, isLossy bool, isDel
 						results[i] = reflect.ValueOf(fmt.Errorf("%s", errMessage))
 					} else {
 						results[i] = reflect.Zero(outType)
+
 					}
 				} else {
+					// for non-error types, we can directly decode into the expected type.
 					ptr := reflect.New(outType)
 					if err := gob.NewDecoder(bytes.NewReader(reply.Reply[i])).DecodeValue(ptr); err != nil {
 						return makeCallerErrorResponse(funcType, "[Caller] failed to decode return value: "+err.Error())
@@ -161,10 +139,12 @@ func CallerStubCreator(serviceInterface any, address string, isLossy bool, isDel
 					results[i] = ptr.Elem()
 				}
 			}
+			// Since there is no failures within the RPC, final return value should be set as a zero RemoteError{}
 			results[returnCount-1] = reflect.Zero(reflect.TypeOf(RemoteError{}))
 			return results
 		})
-		// set the function implementation to the field
+
+		// set the function implementation to the field so that the caller can invoke the function as if it were a local call.
 		reflect.ValueOf(serviceInterface).Elem().FieldByName(funcName).Set(funcImpl)
 	}
 
