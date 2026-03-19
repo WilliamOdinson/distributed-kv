9d0d3a4e7c5f92577621d092c65906c17078982f
WilliamOdinson <109450999+WilliamOdinson@users.noreply.github.com>
2026-03-18 23:01:45 -0400

    refactor: clean up comment

--- raft/types.go
+++ raft/types.go
@@ -13,14 +13,14 @@ const (
 	ElectionTimeoutMax = 300                   // maximum election timeout in ms
 )
 
-// Controller sends to Raft peer at creation time. do not change.
+// Controller sends to Raft peer at creation time. DO NOT CHANGE.
 type RaftSetupInfo struct {
 	Id    int
 	Addr  string
 	Caddr string
 }
 
-// Raft peer must send to Controller on request. do not change.
+// Raft peer must send to Controller on request. DO NOT CHANGE.
 type StatusReport struct {
 	Index     int
 	Term      int
@@ -29,22 +29,20 @@ type StatusReport struct {
 	CallCount int
 }
 
+// LogEntry represents a single entry in the Raft log, containing the term
+// when the entry was received by the leader and the client command.
 type LogEntry struct {
 	Term    int
 	Command []byte
 }
 
-// empty template for the "service interface" that specifies remote calls between Raft peers.
-// it must include the two remote methods needed for the Raft algorithm.
+// RaftInterface defines the service RPC interface for peer-to-peer Raft RPCs.
 type RaftInterface struct {
 	RequestVote   func(term int, candidateId int, lastLogIndex int, lastLogTerm int) (int, bool, remote.RemoteError)
 	AppendEntries func(term int, leaderId int, prevLogIndex int, prevLogTerm int, entries []LogEntry, leaderCommit int) (int, bool, remote.RemoteError)
 }
 
-// complete template for the Control "service interface" that specifies remote calls from
-// Controller to Raft peer. the ControlInterface is active from the moment the Raft peer is
-// created until the Raft peer is no longer needed by the Controller. this interface specifies
-// six remote methods that you must implement. these methods are described later in this file.
+// ControlInterface defines the service RPC interface for Controller-to-peer RPCs.
 type ControlInterface struct {
 	Activate        func() remote.RemoteError
 	Deactivate      func() remote.RemoteError
@@ -54,31 +52,30 @@ type ControlInterface struct {
 	NewCommand      func([]byte) (StatusReport, remote.RemoteError)
 }
 
+// RaftPeer defines the state and RPC stubs for a single Raft peer.
 type RaftPeer struct {
-	mu sync.Mutex
+	// Persistent state on all servers, controlled by Controller
+	id           int  // unique identifier
+	totalPeers   int  // total number of peers in the Raft group
+	isActivate   bool // whether this peer is active
+	isTerminated bool // whether this peer has been terminated
 
-	id           int
-	totalPeers   int
-	isActivate   bool
-	isTerminated bool
-	isLeader     bool
-	isCandidate  bool
+	// Volatile state on all servers, controlled by Raft algorithm
+	isLeader          bool          // whether this peer believes it is the leader
+	isCandidate       bool          // whether this peer is currently a candidate
+	currentTerm       int           // current term number
+	votedFor          int           // candidateId that the peer voted for in current term, default to -1 if none
+	log               []LogEntry    // log entries, indexed starting at 1 (index 0 is dummy)
+	commitIndex       int           // index of highest log entry known to be committed
+	lastApplied       int           // index of highest log entry applied to state machine
+	nextIndex         []int         // each peer's index of the next log entry
+	matchIndex        []int         // each peer's index of highest log entry
+	lastHeartbeatTime time.Time     // last time a heartbeat was received or sent
+	electionTimeout   time.Duration // randomized election timeout for this peer
 
-	currentTerm int
-	votedFor    int
-	log         []LogEntry
-	commitIndex int
-	lastApplied int
-	nextIndex   []int
-	matchIndex  []int
-
-	raftCalleeStub    remote.Callee
-	controlCalleeStub remote.Callee
-	peerStubs         []*RaftInterface
-	ch                chan struct{}
-
-	lastHeartbeatTime     time.Time
-	electionTimeout       time.Duration
-	lastHeartBeatSentTime time.Time
-	randomTimeout         time.Duration
+	mu                sync.Mutex       // mutex to protect current peer's state
+	raftCalleeStub    remote.Callee    // serves RaftInterface RPCs from other peers
+	controlCalleeStub remote.Callee    // serves ControlInterface RPCs from Controller
+	peerStubs         []*RaftInterface // caller stubs to communicate with other peers
+	ch                chan struct{}    // signals NewRaftPeer to return on termination
 }
